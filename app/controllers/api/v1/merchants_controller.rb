37de05231d5919cdf9441b467d54ec0f6124e0b2
Sarah Hall <swatson1994@yahoo.com>
2022-07-12 13:41:52 -0400

    Feat: Add serializer and include in controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.format_merchants(Merchant.all)
   end
 
 end
