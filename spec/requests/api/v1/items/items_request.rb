120a4230b34457476db70fd9987b70f9a854f25d
Sarah Hall <swatson1994@yahoo.com>
2022-07-12 19:55:18 -0400

    Test: Fill in test with required attributes

--- spec/requests/api/v1/items/items_request.rb
+++ spec/requests/api/v1/items/items_request.rb
@@ -8,5 +8,34 @@ RSpec.describe "E-Commerce API: Items" do
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
+
+    expect(items).to be_a(Array)
+    expect(items.count).to eq(12)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
   end
 end
