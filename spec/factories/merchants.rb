bfa5a2b19f82c47dcd50643172b95d03c5243f01
Sarah Hall <swatson1994@yahoo.com>
2022-07-12 13:54:41 -0400

    Remove extra lines

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
-  FactoryBot.define do
-    factory :merchant do
-      name { Faker::JapaneseMedia::StudioGhibli.character }
-    end
+  factory :merchant do
+    name { Faker::JapaneseMedia::StudioGhibli.character }
   end
 end
