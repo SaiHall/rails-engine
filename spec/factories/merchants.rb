d39c10b71f01ad0ecffb9477904bbcdeb2a39894
Sarah Hall <swatson1994@yahoo.com>
2022-07-12 18:02:53 -0400

    Add items factory, add item creation to merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::JapaneseMedia::StudioGhibli.character }
+    after(:create) do |merchant|
+      create_list :item, 4, merchant: merchant
+    end
   end
 end
