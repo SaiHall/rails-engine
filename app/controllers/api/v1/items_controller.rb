e5fb0e72c82159797461f29c80e9eb2ebabef420
Sarah Hall <swatson1994@yahoo.com>
2022-07-13 17:44:47 -0400

    Feat: Add route, private params, and controller create action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,4 +9,13 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.format_item(item)
   end
 
+  def create
+    item = Item.new(item_params)
+    render json: ItemSerializer.format_item(item), status: 201 if item.save
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
