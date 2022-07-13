513357e10543937ec80f89c8ac25272cb4f82450
Sarah Hall <swatson1994@yahoo.com>
2022-07-12 20:30:08 -0400

    Fix: Previous request route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] do
-        namespace :merchants do
-        resources :items, only: [:index]
-      end
-      end
       resources :items, only: [:index]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: 'merchants/items'
+      end
     end
   end
 end
