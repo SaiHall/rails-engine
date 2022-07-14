4602f41df8833b45369a2a40b793d5278c968346
Sarah Hall <swatson1994@yahoo.com>
2022-07-13 20:35:24 -0400

    Refactor: Condense key checks for items

--- spec/requests/api/v1/items/items_request.rb
+++ spec/requests/api/v1/items/items_request.rb
@@ -24,17 +24,12 @@ RSpec.describe "E-Commerce API: Items" do
 
       expect(item).to have_key(:attributes)
       expect(item[:attributes]).to be_a(Hash)
+      expect(item[:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
 
-      expect(item[:attributes]).to have_key(:name)
+
       expect(item[:attributes][:name]).to be_a(String)
-
-      expect(item[:attributes]).to have_key(:description)
       expect(item[:attributes][:description]).to be_a(String)
-
-      expect(item[:attributes]).to have_key(:unit_price)
       expect(item[:attributes][:unit_price]).to be_a(Float)
-
-      expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
     end
   end
