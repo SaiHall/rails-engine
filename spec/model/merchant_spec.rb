55cf3944cd12abd1a7831f27126cd3b97da80cb4
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 16:31:40 -0400

    Test: Move merchant test, create search model method test

--- spec/model/merchant_spec.rb
+++ spec/model/merchant_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it {should have_many :items}
   end
+
+  describe 'class methods' do
+    it 'search method returns similar matches, case insensitive, alphabetically' do
+      jimbob = Merchant.create(name: "Jimbob Dudeguy")
+      jimmy = Merchant.create(name: "Jimmy John")
+      james = Merchant.create(name: "James Rudemenn")
+
+      example = Merchant.search("jim")
+      example2 = Merchant.search("UdE")
+      example3 = Merchant.search("ZlX")
+
+      expect(example).to eq([jimbob, jimmy])
+      expect(example2).to eq([james, jimbob])
+      expect(example3).to eq([])
+    end
+  end
 end
--- spec/requests/api/v1/merchants/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_find_request_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 RSpec.describe "E-Commerce API: Merchant Find" do
   it "Reaches an endpoint successfully" do
     create_list(:merchant, 5)
-    get "/api/v1/merchants/find?=ring"
+    get "/api/v1/merchants/find?name=ring"
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchant = response_body[:data]
   end
 
   xit "Searches merchants for one based off params" do
     create_list(:merchant, 5)
-    get "/api/v1/merchants/find?=ring"
+    get "/api/v1/merchants/find?name=ring"
   end
 end
