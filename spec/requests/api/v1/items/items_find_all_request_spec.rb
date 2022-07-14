47ed1063dca96e9f932daf3e661124c908b58c1e
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 17:53:55 -0400

    Test: Add initial test for item find all

---
+++ spec/requests/api/v1/items/items_find_all_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "E-Commerce API: Items Find All" do
+  it "Reaches an endpoint successfully" do
+    create_list(:merchant, 5)
+    merchant = Merchant.all.first
+    Item.create!(name: "Bubbles", description: "Soapy, shiny, bubbly bubbles", unit_price: 99.90, merchant_id: merchant.id)
+    Item.create!(name: "Cheese", description: "Da cheddar", unit_price: 10.99, merchant_id: merchant.id)
+    Item.create!(name: "Fabreeze", description: "Smells like sunshine", unit_price: 99.90, merchant_id: merchant.id)
+    get "/api/vi/items/find_all?name=ee"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+  end
+end
