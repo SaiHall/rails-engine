b0c1d438b632c8671f3785455e70d17327074e44
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 18:33:44 -0400

    Test: Add test for searching names, excluding ones that don't match

--- spec/requests/api/v1/items/items_find_all_request_spec.rb
+++ spec/requests/api/v1/items/items_find_all_request_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe "E-Commerce API: Items Find All" do
     expect(response).to be_successful
     expect(response.status).to eq(200)
   end
+
+  it "Searches through items for names that match, and returns all that match" do
+    merchant = Merchant.create!(name: "SomeStore SomeStuff")
+    Item.create!(name: "Bubbles", description: "Soapy, shiny, bubbly bubbles", unit_price: 99.90, merchant_id: merchant.id)
+    Item.create!(name: "Cheese", description: "Da cheddar", unit_price: 10.99, merchant_id: merchant.id)
+    Item.create!(name: "Fabreeze", description: "Smells like sunshine", unit_price: 99.90, merchant_id: merchant.id)
+    get "/api/v1/items/find_all?name=ee"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
+
+    expect(items).to be_a(Array)
+    expect(items.count).to eq(2)
+
+    items.each do |item|
+      expect(item[:attributes][:name]).to_not eq("Bubbles")
+    end
+  end
+
+  xit "Searches through items for prices that qualify, and returns all that match" do
+  end
 end
