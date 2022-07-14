c6648185e54e641c92d24a41e901f6011d82223d
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 18:02:21 -0400

    Fix: Add unadded commits

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "merchants/find", to: 'merchants/search#show'
+      get "items/find_all", to: 'items/search#index'
       resources :items, only: [:index, :create, :destroy, :update, :show] do
         resources :merchant, only: [:index], controller: 'items/merchant'
       end
