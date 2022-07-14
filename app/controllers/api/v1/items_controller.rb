5a033a15cd51cb2f22ddb80dc513252f67db25ed
Sarah Hall <swatson1994@yahoo.com>
2022-07-13 20:32:49 -0400

    Feat: Add route and controller action for update

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -23,6 +23,16 @@ class Api::V1::ItemsController < ApplicationController
     item.destroy
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.save
+      render json: ItemSerializer.format_item(item), status: 200
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
