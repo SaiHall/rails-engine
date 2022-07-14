7cad0d32e2cb7590854a30b51948671e2b010006
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 18:25:08 -0400

    Feat: Add search to controller, include search params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def index
+    items = Item.search(search_params)
+    render json: ItemSerializer.format_items(items)
+  end
 
+  private
+  def search_params
+    params.require(:name)
   end
 
 end
