8ee57db7f29ae1996ca813edce7c1596032fe6df
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 18:01:48 -0400

    Fix tests, add route for find all, add controller action

--- spec/requests/api/v1/items/items_find_all_request_spec.rb
+++ spec/requests/api/v1/items/items_find_all_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "E-Commerce API: Items Find All" do
     Item.create!(name: "Bubbles", description: "Soapy, shiny, bubbly bubbles", unit_price: 99.90, merchant_id: merchant.id)
     Item.create!(name: "Cheese", description: "Da cheddar", unit_price: 10.99, merchant_id: merchant.id)
     Item.create!(name: "Fabreeze", description: "Smells like sunshine", unit_price: 99.90, merchant_id: merchant.id)
-    get "/api/vi/items/find_all?name=ee"
+    get "/api/v1/items/find_all?name=ee"
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
