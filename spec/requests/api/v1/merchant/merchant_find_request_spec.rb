40b09a313722bb983d4ca3d6fa2c31789b4b522b
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 13:45:17 -0400

    Fix: Fix path to find merchant

--- spec/requests/api/v1/merchant/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchant/merchant_find_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "E-Commerce API: Merchant Find" do
   it "Reaches an endpoint successfully" do
     create_list(:merchant, 5)
-    get "/api/vi/merchants/find?=ring"
+    get "/api/v1/merchants/find?=ring"
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
@@ -11,6 +11,6 @@ RSpec.describe "E-Commerce API: Merchant Find" do
 
   xit "Searches merchants for one based off params" do
     create_list(:merchant, 5)
-    get "/api/vi/merchants/find?=ring"
+    get "/api/v1/merchants/find?=ring"
   end
 end
