f25963642cad89911318a29f54e0ad5f768a7a1a
Sarah Hall <swatson1994@yahoo.com>
2022-07-13 22:36:47 -0400

    Feat: Add route and controller for merchant from item call

---
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Items::MerchantController < ApplicationController
+
+  def index
+    item = Item.find(params[:item_id])
+    render json: MerchantSerializer.format_merchant(Merchant.find(item.merchant_id))
+  end
+
+end
