a5360c43811d81407bc7e8cca314ed8e4116ecba
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 18:20:43 -0400

    Test: Add test for item search class method

--- spec/model/item_spec.rb
+++ spec/model/item_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'class methods' do
+    it 'search returns array of similarly named items' do
+      merchant = Merchant.create!(name: "SomeStore WithsumStuff")
+      it1 = Item.create!(name: "Bubbles", description: "Soapy, shiny, bubbly bubbles", unit_price: 99.90, merchant_id: merchant.id)
+      it2 = Item.create!(name: "Cheese", description: "Da cheddar", unit_price: 10.99, merchant_id: merchant.id)
+      it3 = Item.create!(name: "Fabreeze", description: "Smells like sunshine", unit_price: 99.90, merchant_id: merchant.id)
+
+      example = Item.search("ee")
+      example2 = Item.search('eS')
+      example3 = Item.search("Yd")
+
+      expect(example).to eq([it2, it3])
+      expect(example2).to eq([it1, it2])
+      expect(example3).to eq([])
+    end
+  end
 end
