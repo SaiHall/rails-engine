18ba763e27a03702d0462ee295c03654c784a377
Sarah Hall <swatson1994@yahoo.com>
2022-07-14 20:01:30 -0400

    Feat: Adjust controller to facilitate max_price

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::Items::SearchController < ApplicationController
   def index
     items = Item.search_name(name_search_params) if params[:name]
     items = Item.search_min(price_search_params) if params[:min_price]
+    items = Item.search_max(price_search_params) if params[:max_price]
+
     render json: ItemSerializer.format_items(items)
   end
 
@@ -12,7 +14,10 @@ class Api::V1::Items::SearchController < ApplicationController
   end
 
   def price_search_params
-    params.require(:min_price)
+    if params[:min_price]
+      params.require(:min_price)
+    elsif params[:max_price]
+      params.require(:max_price)
+    end
   end
-
 end
